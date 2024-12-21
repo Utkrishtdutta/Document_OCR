c496db2630d09814d8bbd77b7b114840032ca03a
Utkrisht Dutta <89606399+Utkrishtdutta@users.noreply.github.com>
2024-12-22 01:19:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import base64
 import streamlit as st
 
 
-def question_query(input, uploaded_files):
+def query_vlm(input, uploaded_files):
     uploaded_files = base64.b64encode(uploaded_files.read()).decode('utf-8')
     response = ollama.chat(
     model="llama3.2-vision",
@@ -21,7 +21,7 @@ uploaded_files = st.file_uploader("Choose a Image file", type=['jpeg','png'])
 if uploaded_files:
     input = st.chat_input("Enter the question")
     if input:
-        output = question_query(input, uploaded_files)
+        output = query_vlm(input, uploaded_files)
         print(output)
         st.write(output)
 
